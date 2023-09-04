187c82d80b4408bffabfd01d729c6971d2dabb0c
Hamza Baig <mail.hamzabaig@gmail.com>
2023-09-04 21:45:48 +0500

    Add text to display content

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 const displayContent = document.getElementById("display-content");
+
+const addToDisplayContainer = () => {
+  displayContent.innerHTML = `<div class="display-content-inner">
+  <h1>Quote of the Day!</h1>
+  <p>
+    “Power is only given to those who are prepared to lower themselves to pick
+    it up.” ― Ragnar Lothbrok
+  </p>
+</div>`;
+};
+
+addToDisplayContainer();
