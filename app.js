5ef5caa2c3a4624189318d638087def1dca67cf5
Hamza Baig <mail.hamzabaig@gmail.com>
2023-09-04 22:02:23 +0500

    Add style to display content quotes

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const displayContent = document.getElementById("display-content");
 
 const addToDisplayContainer = () => {
   displayContent.innerHTML = `<div class="display-content-inner">
-  <h1>Quote of the Day!</h1>
+  <h1>Quotes of the Day!</h1>
   <p>
     “Power is only given to those who are prepared to lower themselves to pick
     it up.” ― Ragnar Lothbrok
+  </p>
+  <p>
+    “Never get discouraged if you fail. Learn from it. Keep trying.” ― Thomas Edison
   </p>
 </div>`;
 };
